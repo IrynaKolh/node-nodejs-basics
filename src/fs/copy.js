7708fc8a8590a7bcca9bf78486d816682ed56fd5
Iryna <iryna.kolhanova@gmail.com>
2022-06-05 11:36:29 -0400

    implement create function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,59 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 export const copy = async () => {
-    // Write your code here 
+    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const destinationWay = path.join(__dirname, 'files-copy');
+    const sourceWay = path.join(__dirname, 'files'); 
+    
+    
+    try {
+        fs.mkdir(destinationWay, {recursive: true}, () => {
+            fs.readdir(sourceWay, (err) => {
+            if (err) {
+                console.log(err);
+            } else {        
+                copyFiles();              
+            }      
+            });
+        });
+           
+          
+          function copyFiles() {
+            fs.readdir(sourceWay, { withFileTypes: true }, (err, files) => {
+              if (err) {
+                console.error(err);
+              } else {
+                files.forEach((file) => {
+                  let sourceFile = path.join(sourceWay, file.name);
+                  let copyFile = path.join(destinationWay, file.name);  
+                  fs.copyFile(sourceFile, copyFile, () => {});        
+                });
+              }
+            });
+          }
+    } catch (error) {
+        console.log ('FS operation failed');
+    }
+
+    // fs.access(sourceWay)
+    // .then(() => {
+    //     fs.mkdir(destinationWay)
+    //     .then(() => {
+    //        files.forEach((file) => {
+    //             let sourceFile = path.join(sourceWay, file.name);
+    //             let copyFile = path.join(destinationWay, file.name);
+    //             fs.copyFile(sourceFile, copyFile, () => {});                                    
+    //         })
+    //     })  
+    //     .catch(() => console.log ('FS operation failed 1'));
+    //     })
+    // .catch(() => console.log ('FS operation failed'));  
 };
+
+copy();
+
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,24 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 export const create = async () => {
-    // Write your code here 
+    // Write your code here   
+    let content = 'I am fresh and young';
+        
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    try {
+        fs.writeFile(filePath, content, {flag: 'wx'}, (err) => {
+            if (err) {
+                throw new Error ('FS operation failed');
+            } 
+            console.log('File is created successfully.');              
+        });          
+    } catch (err) {       
+        throw new Error ('FS operation failed');
+    }
 };
+create();
