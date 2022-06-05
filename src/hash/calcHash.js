ab6f3bb8de2f0162da2b48afef6da42bbc027719
Iryna <iryna.kolhanova@gmail.com>
2022-06-05 15:42:36 -0400

    implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import crypto from 'crypto';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+import { dirname } from 'node:path';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const file = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  try {
+    const text = await fs.readFile(file, { encoding: 'utf8' });
+    const result = crypto.createHash('sha256').update(text).digest('hex');
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
+
+calculateHash();
