5f57e0c9175cc597ad5861eda5541f1d669137eb
Iryna <iryna.kolhanova@gmail.com>
2022-06-05 13:20:22 -0400

    implement env(parse) function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
 export const parseEnv = () => {
     // Write your code here 
+    const {stdout, stdin, exit} = process;
+    stdout.write('Enter 2 variables with prefix RSS_ in format variable_name=variable_value \n');
+    stdin.on('data', data => {
+        const values = data.toString().split(' ');
+        stdout.write(`${values[0]} ; ${values[1]}`);
+        process.exit();
+    });
+    process.on('SIGINT', () => {  
+        process.stdout.write('Buy!');
+        process.exit();
+    });
 };
+parseEnv();
